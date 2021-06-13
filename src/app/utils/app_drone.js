4b0068c0fdb625ee57f1bc6b780b4af506ec7440
Thierry Kg <t.kaing13@gmail.com>
2021-06-13 23:48:24 +0200

    - afficher le flux video dans l'appli

--- src/app/utils/app_drone.js
+++ src/app/utils/app_drone.js
@@ -5,17 +5,12 @@ const run = (command) => {
     const socket = dgram.createSocket({ type: 'udp4', debug: true });
     console.log(socket);
     socket.bind(8001);
-    socket.on('close', (msg, info) => {
-        console.log('Close', msg);
-    });
-    socket.on('message', (msg, info) => {
-        console.log('Message', msg);
-    });
+    socket.on('close', (msg, info) => console.log('Close', msg));
+    socket.on('message', (msg, info) => console.log('Message', msg));
     socket.once('listening', () => {
         console.log(command);
         socket.send(command, undefined, undefined, 8889, '192.168.10.1', (err) => {
             if (err) throw err
-            //socket.close();
         });
     });
 };
--- src/screens/FpvScreen.js
+++ src/screens/FpvScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button, Container, Text, View } from "native-base";
+import { Button, Container, Text, View, Thumbnail } from "native-base";
 import FpvRemote from "./FpvScreen/FpvRemote";
 import { lockAsync, OrientationLock } from "expo-screen-orientation";
 import Gamepad from "./GamepadScreen/Gamepad";
@@ -12,7 +12,10 @@ const FpvScreen = (
     { navigation }
 ) => {
 
+    const [openStream, setOpenStream] = useState(false);
+    const [currentFrame, setCurrentFrame] = useState(null);
     const [isFpvRemoteView, setFpvRemoteView] = useState(true);
+    const [currentInterval, setCurrentInterval] = useState(null);
 
     const ffmpeg = {
         core: async () => {
@@ -35,61 +38,93 @@ const FpvScreen = (
                 console.log('Permission Denied.');
             }
         },
+        close: () => {
+            RNFFmpeg.cancel();
+            setOpenStream(false);
+        },
         input: () => {
             //let input = `file://${ RNFS.DownloadDirectoryPath }/sample-mp4-file.mp4`;
-            //let input = "udp://0.0.0.0:11111";
-            //let input = "udp://192.168.10.1:11111";
-            let input = "udp://127.0.0.1:11111";
+            //let input = `file://${ RNFS.DownloadDirectoryPath }/sample-video.mp4`;
+            //let input = "udp://0.0.0.0:11111"; // ???
+            let input = "udp://127.0.0.1:11111"; // OK
+            //let input = "udp://192.168.10.1:11111"; // ???
             return input;
         },
         output: () => {
-            let output = `file://${ RNFS.DownloadDirectoryPath }/outputbis.mp4`;
+            let output = `file://${ RNFS.DownloadDirectoryPath }/output_frame.jpg`;
             return output;
         },
         config: () => {
             RNFFmpegConfig.enableLogCallback(
-                (log) =>
-                    console.log(`Logs; ${log.executionId}:${log.message}`)
+                (log) => {
+                    //console.log(`Logs; ${log.executionId}:${log.message}`);
+                }
             );
             RNFFmpegConfig.enableStatisticsCallback(
-                (statistics) =>
-                    console.log(`Statistics; executionId: ${statistics.executionId}, video frame number: ${statistics.videoFrameNumber}, video fps: ${statistics.videoFps}, video quality: ${statistics.videoQuality}, size: ${statistics.size}, time: ${statistics.time}, bitrate: ${statistics.bitrate}, speed: ${statistics.speed}`)
+                (statistics) => {
+                    //console.log("Statistics; XXX", statistics);
+                    //setListOfFrames([...listOfFrames]);
+                    //console.log(`Statistics; executionId: ${statistics.executionId}, video frame number: ${statistics.videoFrameNumber}, video fps: ${statistics.videoFps}, video quality: ${statistics.videoQuality}, size: ${statistics.size}, time: ${statistics.time}, bitrate: ${statistics.bitrate}, speed: ${statistics.speed}`);
+
+                }
             );
         },
-        instance: () => {
-            //const options = '-r 30 -s 960x720 -codec:v mpeg1video -b:v 800k -f h264';
-            const options = '-vcodec copy';
-            const command = `-i ${ ffmpeg.input() } ${ options } ${ ffmpeg.output() }`;
-            console.log(command);
+        instance: async () => {
 
-            const socket = dgram.createSocket({ type: 'udp4', debug: true });
-            console.log(socket);
-            socket.bind(8001);
+            // Full command
+            // = full cmd from api = '-r 30 -s 960x720 -codec:v mpeg1video -b:v 800k -f h264'
+            // = override single image = '-q:v 4 -r 1 -update 1'
+            // Ne pas utiliser
+            // = -vf fps=1/X
+            // Informations utiles
+            // = qscale:v = qualité vidéo (1-31)
+            // = R = '-r 1/20';
+            // = R = '-r 30 -f image2';
 
-            socket.on('close', (msg, info) => {
-                console.log('Close', msg);
-            });
-            socket.on('message', (msg, info) => {
-                console.log('Message', msg);
-            });
-            socket.once('listening', () => {
-                ffmpeg.config();
-                /*RNFFmpeg.executeAsync(command, function (completedExec) {
-                    console.log(completedExec);
-                })
-                    .then(executionId => console.log(`Async FFmpeg process started with executionId ${executionId}.`))
-                    .catch(reason => console.log(reason))
-                ;*/
-                RNFFmpeg.executeAsync(command, (execution) => console.log(execution))
-                    .then(executionId => console.log(`Async FFmpeg process started with executionId ${executionId}.`))
-                    .catch(reason => console.log(reason))
-                ;
-            });
+            const exists = await RNFS.exists(ffmpeg.output());
+
+            if (exists) {
+                console.log('Supprimer fichier d\'abord.');
+            } else {
+
+                const socket = dgram.createSocket({type: 'udp4', debug: true});
+                const command = `-i ${ffmpeg.input()} -q:v 4 -r 1 -update 1 ${ffmpeg.output()}`;
+
+                socket.bind(8001);
+                socket.on('close', (msg, info) => {
+                    console.log('Close', msg);
+                    setOpenStream(false);
+                });
+                socket.on('message', (msg, info) => console.log('Message', msg));
+                socket.once('listening', () => {
+                    ffmpeg.config();
+                    RNFFmpeg.executeAsync(command, (execution) => {
+                        console.log(execution);
+                        setOpenStream(false);
+                    })
+                        .then(executionId => {
+                            console.log(`Async FFmpeg process started with executionId ${executionId}.`);
+                            setOpenStream(true);
+                        })
+                        .catch(reason => {
+                            console.log(reason);
+                            setOpenStream(false);
+                        })
+                    ;
+                });
+            }
+        },
+        toggleFrame: () => {
+            setCurrentFrame(ffmpeg.output() + '?' + Date.now());
         }
     };
 
     useEffect(() => {
         lockAsync(OrientationLock.LANDSCAPE_LEFT);
+        return () => {
+            clearInterval(currentInterval);
+            setCurrentInterval(null);
+        }
     }, []);
 
     useEffect(() => {
@@ -97,6 +132,20 @@ const FpvScreen = (
             lockAsync(OrientationLock.LANDSCAPE_LEFT);
         });
     }, [navigation]);
+
+    useEffect(() => {
+        if (openStream) {
+            setCurrentInterval(setInterval(ffmpeg.toggleFrame, 1500));
+        } else {
+            setCurrentFrame(null);
+            clearInterval(currentInterval);
+            setCurrentInterval(null);
+        }
+    }, [openStream]);
+
+    useEffect(() => {
+        console.log('CURRENT FRAME', currentFrame);
+    }, [currentFrame]);
 
     return (
         <Container>
@@ -106,33 +155,22 @@ const FpvScreen = (
                     <View style={{ ...styles.padView }}>
                         <View style={{ ...styles.streamView }}>
 
-                            <Button onPress={ async () => await ffmpeg.core() } block danger rounded>
-                                <Text>Start FFMPEG</Text>
+                            <Button onPress={ async () => await ffmpeg.core() }
+                                    block danger rounded>
+                                <Text>Run FFMPEG</Text>
                             </Button>
 
-                            <Button onPress={ () => RNFFmpeg.cancel() } block danger rounded>
-                                <Text>Stop FFMPEG</Text>
+                            <Button onPress={ () => ffmpeg.close() }
+                                    block danger rounded>
+                                <Text>Close FFMPEG</Text>
                             </Button>
 
-                            {/*<Button onPress={ () => {
-                                console.log('READ IMG');
-                                RNFS.readFile(output, 'ascii')
-                                    .then(result => console.log('READ IMG = SUCCESS', result))
-                                    .catch(failure => console.log('READ IMG = FAILURE', failure));
-                            }}>
-                                <Text>READ IMG</Text>
-                            </Button>
-
-                            <Button onPress={ async () => {
-                                console.log('WRITE IMG');
-                                RNFS.writeFile(output, 'xxx',  'ascii')
-                                    .then(result => console.log('WRITE IMG = SUCCESS', result))
-                                    .catch(failure => console.log('WRITE IMG = FAILURE', failure));
-                            }}>
-                                <Text>WRITE IMG</Text>
-                            </Button>*/}
+                            { openStream && currentFrame &&
+                                <Thumbnail square large source={{ uri: currentFrame }}
+                                           style={{ width: 300, height: 200 }}
+                                           fadeDuration={ 0 } />
+                            }
                         </View>
-
                         <View style={{ ...styles.remoteView }}>
                             <FpvRemote setFpvRemoteView={ setFpvRemoteView } />
                         </View>
