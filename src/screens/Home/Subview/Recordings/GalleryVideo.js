f4d657501668f35592ff295b52691e777cc25ac6
DorianMenard <47523177+DorianMenard@users.noreply.github.com>
2021-07-01 17:23:30 +0200

    Share on video

--- src/screens/Home/Subview/Recordings/GalleryVideo.js
+++ src/screens/Home/Subview/Recordings/GalleryVideo.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-import { Dimensions, StyleSheet } from "react-native";
-import { Image, ScrollView, Wrap } from "native-base";
+import { Dimensions, StyleSheet, TouchableOpacity } from "react-native";
+import { Image, ScrollView, Wrap, Column, Box, Row } from "native-base";
 import Video from 'react-native-video';
+import Color from "../../../../App/Color"
+import { on } from "../../../../tools"
 
 const GalleryVideo = (
     {
@@ -14,25 +16,41 @@ const GalleryVideo = (
 
     return (
         <ScrollView>
-            { listOfVideos.map(_it =>
-                <Video
-                    source={{ uri: `file://${ _it.path }` }}
-                    resizeMode="cover"
-                    autoplay={false}
-                    controls={true}
-                    ref={(ref) => {
-                        this.player = ref
-                    }}
-                    style={ styles.backgroundVideo }
-                />                  
-            ) }
+            
+                { listOfVideos.map((_it, index) =>
+                    <Row flex={1} height={250}>
+                        <Video
+                            source={{ uri: `file://${ _it.path }` }}
+                            resizeMode="cover"
+                            autoplay={false}
+                            controls={true}
+                            ref={(ref) => {
+                                this.player = ref
+                            }}
+                            style={ styles.backgroundVideo }
+                        />
+
+                        <TouchableOpacity key={index} onPress={() => {
+                            console.log("OnPress")
+                            on.home.recordings.share({url: `file://${ _it.path }`})
+                            }}>
+
+                            <Box bg={ Color.blue } height={250} width={50}/>   
+
+                        </TouchableOpacity> 
+                                            
+                          
+                    </Row>
+      
+                ) }
+            
         </ScrollView>
     );
 };
 
 var styles = StyleSheet.create({
     backgroundVideo: {
-      width: Dimensions.get('window').width,
+      width: Dimensions.get('window').width - 50,
       height: 250
     },
   });
