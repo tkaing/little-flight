ff7e6fabb0dc8175bb0898cd6314dbdb5186dd6a
Ken Mavoungou <35596468+kenjorion@users.noreply.github.com>
2021-07-02 21:08:30 +0200

    Update Parameters.js

--- src/Api/Parameters.js
+++ src/Api/Parameters.js
@@ -1,5 +1,5 @@
-//const API_URL = 'https://arctic-toque-79278.herokuapp.com';
-const API_URL = 'http://10.33.2.180:3000';
+const API_URL = 'https://arctic-toque-79278.herokuapp.com';
+//const API_URL = 'http://10.33.2.180:3000';
 const API_GOOGLE = 'https://www.googleapis.com/oauth2/v3';
 
 export {
